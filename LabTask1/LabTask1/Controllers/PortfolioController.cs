1b14f7f8a8ffd47eb16eeff784fb1c6c339d807b
Sayem2004 <mdalimransayem@gmail.com>
2026-02-17 20:58:01 +0600

    Basic

---
+++ LabTask1/LabTask1/Controllers/PortfolioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LabTask1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
