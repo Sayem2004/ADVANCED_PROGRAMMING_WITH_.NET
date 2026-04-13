a1f63263e3db0222718c5b60a1e4861cb54af9d2
Sayem2004 <mdalimransayem@gmail.com>
2026-04-13 23:46:26 +0600

    DataBase Fast Try

---
+++ Basic/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            var student = new Student
+            {
+                Name = "John",
+                Age = 22
+            };
+
+            return View(student);
+        }
+    }
+}
