f4b9176c29726656eafc6d3bdf5781ae43414d59
Sayem2004 <mdalimransayem@gmail.com>
2026-04-27 09:35:40 +0600

    Clean .NET project

---
+++ Auth/Auth/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Auth.DTOs;
+using Auth.EF;
+using Auth.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Auth.Controllers
+{
+    public class AuthController : Controller
+    {
+        AuthCSp26Context db;
+
+        public AuthController(AuthCSp26Context db) { 
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View(new RegDTO() { });
+        }
+        [HttpPost]
+        public IActionResult Registration(RegDTO obj) {
+            if (ModelState.IsValid) {
+                var user = new User() { 
+                    Name = obj.Name,
+                    Email = obj.Email,
+                    Password = GetMd5(obj.Password),
+                    Username = obj.Username,
+                    Type = 2
+                };
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+            return View(obj);
+        }
+        string GetMd5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    sb.Append(b.ToString("x2")); // lowercase hex
+                }
+
+                return sb.ToString();
+            }
+        }
+
+    }
+}
